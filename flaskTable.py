8d8f7dc43f70ea00b428e6e5190cff3b5f59a4a6
Jorn-Tumrongwit <jorn.tum@studen.mahidol.edu>
2021-02-09 12:54:27 +0700

    fixed for some reason

--- flaskTable.py
+++ flaskTable.py
@@ -3,23 +3,24 @@ import mysql.connector
 
 app = Flask(__name__)
 
+@app.route("/")
 def showCustomerTable():
     myTable = '''
-<!DOCTYPE html>
-<html>
-<head>
-<scipt src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <scipt src="http://code.jquery.com/jquery-latest.min.js"></script>
 
-</head>
-<body>
-<h1>Customers</h1>
-<table style="width:100">
-    <tr>
-        <th>id</th>
-        <th>Name</th>
-        <th>Address</th>
-    </tr>
-'''
+    </head>
+    <body>
+    <h1>Customers</h1>
+    <table style="width:100">
+        <tr>
+            <th>id</th>
+            <th>Name</th>
+            <th>Address</th>
+        </tr>
+    '''
     mydb = mysql.connector.connect(
         host="localhost",
         user="root",
@@ -35,22 +36,21 @@ def showCustomerTable():
     myresultShow = mycursor.fetchall()
     for row in myresultShow:
         myTable = myTable + '''
-    <tr>
-        <td>{}</td>
-        <td>{}</td>
-        <td>{}</td>
-    </tr>
-'''.format(row[0],row[1],row[2])
+        <tr>
+            <td>{}</td>
+            <td>{}</td>
+            <td>{}</td>
+        </tr>
+        '''.format(row[0],row[1],row[2])
         print(row)
         
     myTable = myTable + '''
-</body>
-</html>
-'''
+    </body>
+    </html>
+    '''
     
     return myTable
 
-print(showCustomerTable())
 
-if(__name__) == "__main__":
-    app.run
+if __name__ == "__main__":
+    app.run()
